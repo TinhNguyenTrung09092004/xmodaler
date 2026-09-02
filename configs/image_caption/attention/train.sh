a02c21c3546970fbf411e367b378ad07db279820
tinh <tinh87250@gmail.com>
2026-09-02 21:35:19 +0700

    only cosnet

--- configs/image_caption/attention/train.sh
+++
@@ -1,5 +0,0 @@
-# XE
-python3 train_net.py --num-gpus 4 --config-file ./configs/image_caption/attention/attention.yaml
-
-# RL
-python3 train_net.py --num-gpus 4 --config-file ./configs/image_caption/attention/attention_rl.yaml
--- configs/image_caption/meshed_memory/train.sh
+++
@@ -1,5 +0,0 @@
-# XE
-python3 train_net.py --num-gpus 4 --config-file configs/image_caption/meshed_memory/meshed_memory.yaml
-
-# RL
-python3 train_net.py --num-gpus 4 --config-file configs/image_caption/meshed_memory/meshed_memory_rl.yaml
